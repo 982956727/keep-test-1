b50008f59804503aa20e045c709d1f8c4b10982a
982956727 <982956727@qq.com>
2023-11-18 09:31:22 +0800

    创建 Icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="nav">
-      <router-link to="/money">记账</router-link>
+      <router-link to="/money">
+        <Icon name="money" />
+        记账
+      </router-link>
       |
-      <router-link to="/labels">标签</router-link>
+      <router-link to="/labels">
+        <Icon name="labels" />
+        标签
+      </router-link>
       |
-      <router-link to="/statistics">统计</router-link>
+      <router-link to="/statistics">
+        <Icon name="statistics" />
+        统计
+      </router-link>
   </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-
-  export default Vue.extend({
-    
+  let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+  try {
+      importAll(require.context('../assets/icons',true,/\.svg$/))
+  }catch(error) {
+    console.log(error)
+  }
+  export default ({
+    name: 'Nav',
   })
 </script>
 
