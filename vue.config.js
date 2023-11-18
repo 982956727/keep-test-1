d1c3b56b7744e0a1c1c83a075ee0eb4f38e12365
982956727 <982956727@qq.com>
2023-11-18 11:30:08 +0800

    active-class 和 更新 meta:vp 和  解决了 SVG 自带 fill 属性的问题

--- vue.config.js
+++ vue.config.js
@@ -10,15 +10,14 @@ module.exports = {
       .test(/\.(svg)(\?.*)?$/)
       .include.add(dir).end()
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+      .use('svgo-loader').loader('svgo-loader')
+      .tap((options) => ({options,plugins: [{ removeAttrs: { attrs: "fill" } }],})).end()   
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'),[{plainSprite: true}])
     config.module.rule("svg").exclude.add(dir);//其他 svg loader 排除 icons 目录
 
     // use("svgo-loader")
       // .loader("svgo-loader")
-      // .tap((options) => ({
-      //   ...options,
-      //   plugins: [{ removeAttrs: { attrs: "fill" } }],
-      // }))
+      // .tap((options) => ({options,plugins: [{ removeAttrs: { attrs: "fill" } }],}))
       // .end();
 
   }
