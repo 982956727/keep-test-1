1b0fe6cdd1d961e2bd508fae13cef73830841cfa
982956727 <982956727@qq.com>
2023-12-05 12:29:07 +0800

    删除 Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,6 @@
 <template>
     <Layout class-prefix="layout">
       <NumberPad :value.sync="record.amount" @submit = 'saveRecord' />
-      <Types :value.sync="record.type" />
       <Tabs :data-source = 'recordTypeList' :value.sync="record.type" />
       <div class="notes">
         <FormItem field-name="备注" 
@@ -16,15 +15,15 @@
 <script lang="ts">
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import { Component } from 'vue-property-decorator';
   import { RecordItem } from '@/custom';
   import recordTypeList from '@/constants/recordTypeList'
+  import Tabs from '@/components/Tabs.vue'
 
   @Component({
-    components:{ NumberPad,Types,FormItem,Tags },
+    components:{ NumberPad,FormItem,Tags,Tabs },
   })
   export default class Money extends Vue {
     get recordList() {
